e9ac638d430421f46c28a29c35216abf754df677
msroda <marcinsroda94@gmail.com>
2015-06-11 21:24:17 +0200

    Lista->wektor
    Lista staje się wektorem. I i tak dupa...

--- TEST/CircularList.h
+++ TEST/CircularList.h
@@ -4,21 +4,21 @@
 
 template <typename t>
 class  CircularList{
-	std::list < t > coreList;
+	std::vector < t > coreList;
 public:
 	void operator+=(t data){
 		coreList.push_back(data);
 	}
-	typename std::list<t>::iterator begin(){ return coreList.begin(); }
-	typename std::list<t>::iterator end(){ return coreList.end(); }
+	typename std::vector<t>::iterator begin(){ return coreList.begin(); }
+	typename std::vector<t>::iterator end(){ return coreList.end(); }
 	void rndm()
 	{
 		std::random_shuffle(coreList.begin(), coreList.end());
 	}
 	class CircularIterator{
-		typename std::list< t >::iterator pointer;
-		typename std::list< t >::iterator begin;
-		typename std::list< t >::iterator end;
+		typename std::vector< t >::iterator pointer;
+		typename std::vector< t >::iterator begin;
+		typename std::vector< t >::iterator end;
 	public:
 		CircularIterator(CircularList<t>& list){pointer = list.begin(); end = list.end(); begin = list.begin(); }
 		CircularIterator(){};
